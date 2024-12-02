031b5fc4c6b2f0eaebd72335658f0a7b668ce6bb
maitrygajiwala <maitry22gajiwala@gmail.com>
2024-12-02 18:42:08 -0500

    created frontend

---
+++ frontend/src/components/CreateTodo.jsx
@@ -0,0 +1,7 @@
+export function Todo(){
+    return <div>
+        <input type="text" placeholder="title"/> <br />
+        <input type="text" placeholder="description"/> <br />
+        <button>Add a todo</button>
+    </div>
+}
